9efee59f8f8439616ae73460154d8a48e235b59c
SirRGB <sirrgb@amyrom.ml>
2022-09-22 18:06:13 +0200

    msm8998-common: amy-fy

--- common.mk
+++ common.mk
@@ -31,7 +31,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-amy
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
